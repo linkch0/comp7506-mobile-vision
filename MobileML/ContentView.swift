3f8aea01dca061e624fc4b1969a7f7b2091873ca
Caffretro <caffretro@gmail.com>
2023-04-19 23:41:36 +0800

    updated circle return button

--- MobileML/ContentView.swift
+++ MobileML/ContentView.swift
@@ -15,8 +15,22 @@ struct ContentView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        Button("Homepage") {
-            presentationMode.wrappedValue.dismiss()
+        HStack {
+            Spacer()
+            Button(action: {presentationMode.wrappedValue.dismiss()}, label: {
+                    Circle()
+                        .fill(Color(.secondarySystemBackground))
+                        .frame(width: 30, height: 30) // You can make this whatever size, but keep UX in mind.
+                        .overlay(
+                            Image(systemName: "xmark")
+                                .font(.system(size: 15, weight: .bold, design: .rounded)) // This should be less than the frame of the circle
+                                .foregroundColor(.secondary)
+                        )
+                })
+                .buttonStyle(PlainButtonStyle()) // This gives it no designs on idle, but can change on input
+                .accessibilityLabel(Text("Close")) // Keep it accessible
+                .padding(.horizontal, 25)
+                .padding(.top, 20)
         }
         // Swiper for cards
         TabView {
